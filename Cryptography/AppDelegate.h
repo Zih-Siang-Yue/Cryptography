df60f8dc55190fea438e8f730b51417ab37ff155
Sean Yue <sean.yue@skylinetw.com>
2019-06-25 17:30:28 +0800

    first commit

---
+++ Cryptography/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Cryptography
+//
+//  Created by Sean.Yue on 2019/6/25.
+//  Copyright © 2019 Sean.Yue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Cryptography/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Cryptography
+//
+//  Created by Sean.Yue on 2019/6/25.
+//  Copyright © 2019 Sean.Yue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
